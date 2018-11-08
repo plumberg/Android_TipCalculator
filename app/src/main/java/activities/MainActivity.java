0dad5cb671754c233c008434fdf6db60ad781dd6
plumberg <hana.ryzhuk@gmail.com>
2018-11-08 16:09:44 -0500

    showAbout menu item updated

--- app/src/main/java/activities/MainActivity.java
+++ app/src/main/java/activities/MainActivity.java
@@ -1,12 +1,16 @@
 package activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.sip.SipSession;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
@@ -233,7 +237,17 @@ public class MainActivity extends AppCompatActivity
 
     public void showAbout(MenuItem item) {
         //called when user clicks About
-        Toast.makeText(getApplicationContext(),"About...",Toast.LENGTH_LONG).show();
-
+       // Utils.showInfoDialog(this,"About","App created in a class");
+        AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        adb.setIcon(ContextCompat.getDrawable(this,R.mipmap.ic_launcher));
+        adb.setTitle("About...").setMessage("App created in a class");
+        adb.setCancelable(true);
+        adb.setNeutralButton(getString(android.R.string.ok), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+               // Toast.makeText(this, "Clicked OK", Toast.LENGTH_SHORT).show();
+            }
+        });
+        adb.show();
     }
 }
